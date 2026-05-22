bf4220dd9ce668e88b3b1e3bd77c2cfc5174540a
bluehhhh-byte <bluehhhh@naver.com>
2026-05-23 06:25:08 +0900

    remove: 재생목록에서 임베드 불가 영상 3개 제거

--- radio/playlist.js
+++ radio/playlist.js
@@ -1,11 +1,8 @@
 var PLAYLIST_IDS = [
-  'Mz031oU0Xfw',
   'CCNI66vlzZE',
   'FNRkH_rTlIk',
   'YBzJ0jmHv-4',
   'jIIdLgUbj5E',
   'NrfikKxF4Ps',
-  'XLNlzHY_SFs',
-  'pNRSXxI4fdM',
-  'dWZs4hOtJ4I'
+  'XLNlzHY_SFs'
 ];
