7c60ba122649c0bad02bd7808b1de1821959c772
bluehhhh <bluehhhh@users.noreply.gitlab.com>
2026-05-23 06:04:07 +0900

    fix: loadPlaylist에 비디오 ID 배열 사용, 랜덤 인덱스 시작

--- radio/playlist.js
+++ radio/playlist.js
@@ -1,8 +1,11 @@
-var PLAYLIST = [
-  { id:'XLNlzHY_SFs', title:'Bored (Bored)', artist:'Jeff Bernat',
-    comment:'심심한 하루도, 네가 떠오르면 그냥 견딜 만해져' },
-  { id:'pNRSXxI4fdM', title:'흔들리는 꽃들 속에서 네 샴푸향이 느껴진거야', artist:'장범준',
-    comment:'봄바람 같은 사람아, 네가 없는 계절도 결국 지나간다' },
-  { id:'dWZs4hOtJ4I', title:'밤편지', artist:'아이유 (IU)',
-    comment:'잠들지 못하는 밤, 이 노래가 너의 창가에 닿기를' }
+var PLAYLIST_IDS = [
+  'Mz031oU0Xfw',
+  'CCNI66vlzZE',
+  'FNRkH_rTlIk',
+  'YBzJ0jmHv-4',
+  'jIIdLgUbj5E',
+  'NrfikKxF4Ps',
+  'XLNlzHY_SFs',
+  'pNRSXxI4fdM',
+  'dWZs4hOtJ4I'
 ];
